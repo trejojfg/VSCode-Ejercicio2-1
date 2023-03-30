74a2ed82b56e1f8ff0f3ad876161382baad0847a
TREJO <trejojfrancisco@gmail.com>
2023-03-30 13:21:51 +0200

    primera carga

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para solicitar dos números y luego calcule y emita la suma en pantalla.
+            
+            int n1, n2, R;
+            Console.WriteLine("Introducir 1º Número");
+            n1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Introducir 2º Número");
+            n2 = int.Parse(Console.ReadLine());
+            R = n1 + n2;
+            Console.WriteLine("El Resultado es " + R);
+
+        }
+    }
+}
